04ef8d1117f93e053d0e1ae6594acbf261ea4edb
Dhiogoandre <dhiogoandre26@gmail.com>
2025-06-26 22:45:22 -0400

    refactor: logos navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user! # Autentica o usuário antes de qualquer ação
   include Pagy::Backend # Inclui Pagy para uso no Backend
 
-  before_action :set_layout_by_controlle
-  private
-
-  def set_layout_by_controller
-    if devise_controller?
-      self.class.layout "devise_application"
-    elsif controller_name == "home"
-      self.class.layout "home_application"
-    else
-      self.class.layout "application"
-    end
-  end
+ # before_action :set_layout_by_controller
+ # private
+#
+ # def set_layout_by_controller
+ #   if devise_controller?
+ #     self.class.layout "devise_application"
+ #   elsif controller_name == "home"
+ #     self.class.layout "home_application"
+ #   else
+ #     self.class.layout "application"
+ #   end
+ # end
 end
