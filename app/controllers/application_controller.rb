efdcc57388ca8809e7b9fa085bcf5a51407eb362
Dhiogoandre <dhiogoandre26@gmail.com>
2025-06-26 23:03:18 -0400

    refactor: concern pra layout de usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,11 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user! # Autentica o usuário antes de qualquer ação
-  include Pagy::Backend # Inclui Pagy para uso no Backend
+  # before_action :authenticate_user!
+  include Pagy::Backend
+  include LayoutByUser
 
- # before_action :set_layout_by_controller
- # private
-#
- # def set_layout_by_controller
- #   if devise_controller?
- #     self.class.layout "devise_application"
- #   elsif controller_name == "home"
- #     self.class.layout "home_application"
- #   else
- #     self.class.layout "application"
- #   end
- # end
+  private
+
+  # def after_sign_in_path_for(resource)
+  #   stored_location_for(resource) || root_path
+  # end
 end
