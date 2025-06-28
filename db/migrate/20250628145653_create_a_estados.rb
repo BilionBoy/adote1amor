3148f93d4bd2383254d2980806051dad3cb276fc
Dhiogoandre <dhiogoandre26@gmail.com>
2025-06-28 11:35:27 -0400

    refactor: criando partials e customizando landing page

--- db/migrate/20250628145653_create_a_estados.rb
+++ db/migrate/20250628145653_create_a_estados.rb
@@ -4,10 +4,10 @@ class CreateAEstados < ActiveRecord::Migration[7.2]
   def up
    unless table_exists?(:a_estados) 
     create_table :a_estados do |t|
-      t.string :descricao
-      t.string :uf
-      t.string :created_by
-      t.string :updated_by
+      t.string   :descricao
+      t.string   :uf
+      t.string   :created_by
+      t.string   :updated_by
       t.datetime :deleted_at
       t.timestamps
     end
--- db/migrate/20250628145735_create_a_cidades.rb
+++ db/migrate/20250628145735_create_a_cidades.rb
@@ -3,13 +3,12 @@
 class CreateACidades < ActiveRecord::Migration[7.2]
   def up
    unless table_exists?(:a_cidades)
-    create_table :a_cidades do |t|
-      t.string :descricao
+    create_table   :a_cidades do |t|
+      t.string     :descricao
       t.references :a_estado
-          
-      t.string :created_by
-      t.string :updated_by
-      t.datetime :deleted_at
+      t.string     :created_by
+      t.string     :updated_by
+      t.datetime   :deleted_at
       t.timestamps
     end
    end
